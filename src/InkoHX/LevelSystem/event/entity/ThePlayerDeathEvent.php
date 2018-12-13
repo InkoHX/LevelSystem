530e160a8572c4301c9faf5af0596fad7c34a17f
InkoHX <inkohx@gmail.com>
2018-12-14 02:15:18 +0900

    update

---
+++ src/InkoHX/LevelSystem/event/entity/ThePlayerDeathEvent.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: InkoHX
+ * Date: 2018/12/14
+ * Time: 2:04
+ */
+
+namespace InkoHX\LevelSystem\event\entity;
+
+use InkoHX\LeveLibrary\LevelAPI;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\Player;
+
+class ThePlayerDeathEvent implements Listener
+{
+    public function event(PlayerDeathEvent $event)
+    {
+        $player = $event->getPlayer();
+        $cause = $player->getLastDamageCause();
+        if (!$cause instanceof EntityDamageByEntityEvent) return;
+        $damager = $cause->getDamager();
+        if (!$damager instanceof Player) return;
+        LevelAPI::Auto($player);
+    }
+}
---
+++ src/InkoHX/LevelSystem/event/level/LevelUpEvent.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: InkoHX
+ * Date: 2018/12/14
+ * Time: 1:57
+ */
+
+namespace InkoHX\LevelSystem\event\level;
+
+
+use InkoHX\LeveLibrary\event\level\PlayerLevelUpEvent;
+use pocketmine\event\Listener;
+
+class LevelUpEvent implements Listener
+{
+    public function event(PlayerLevelUpEvent $event)
+    {
+        $event->getPlayer()->sendMessage("§l§bLEVEL UP!!§r {$event->getOldLevel()} §c➡§r {$event->getNewLevel()}");
+    }
+}
